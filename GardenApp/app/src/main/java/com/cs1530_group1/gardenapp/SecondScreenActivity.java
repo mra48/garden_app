5ca0e855d07a2831a8a5c53aa3f68bfc54950e4b
Charles Smith <chuckie512@gmail.com>
2015-05-22 19:32:37 -0400

    added up button on SecondScreenActivity

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/SecondScreenActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/SecondScreenActivity.java
@@ -12,6 +12,8 @@ public class SecondScreenActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
