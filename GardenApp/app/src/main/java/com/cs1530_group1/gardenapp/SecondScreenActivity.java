a916f6abcf65a69f5044ba691d3812ed54244ef8
Charles Smith <chuckie512@gmail.com>
2015-05-22 19:07:11 -0400

    created a second activity

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/SecondScreenActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/SecondScreenActivity.java
@@ -1,25 +1,23 @@
 package com.cs1530_group1.gardenapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SecondScreenActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second_screen, menu);
         return true;
     }
 
@@ -36,11 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void changeToScreen2 (View veiw){
-        Intent intent = new Intent(this, SecondScreenActivity.class);
-        startActivity(intent);
-
     }
 }
