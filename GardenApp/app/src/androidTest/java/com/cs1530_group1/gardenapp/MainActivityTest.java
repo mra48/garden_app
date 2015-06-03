bd209935ef8ad4667b22747e9ec2d5da288c8f53
Charles Smith <cas275@pitt.edu>
2015-06-03 14:34:12 -0400

    added a base JUnit test, to see if JUint works correctly

---
+++ GardenApp/app/src/androidTest/java/com/cs1530_group1/gardenapp/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.cs1530_group1.gardenapp;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by cas275 on 6/3/15.
+ */
+public class MainActivityTest extends TestCase {
+
+    /**
+     * Tests if the JUnit will run
+     */
+
+    public void testExists(){
+        assertTrue(true);
+    }
+
+}
