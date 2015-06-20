26bf20015c50e7487658421685f72c4ca8c97505
Muneeb Alvi <mra48@users.noreply.github.com>
2015-06-20 00:39:47 -0400

    Finished Basic Interface and Testing

---
+++ GardenApp/app/src/garden/java/com/cs1530_group1/gardenapp/Species.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muneeb
+ */
+public class Species {
+ 
+    String type;
+    String sun;
+    int prune;
+    int water;
+    int fertilize;
+    
+    public Species(String type, String sun, int prune, int water, int fertilize)
+    {
+        this.type = type;
+        this.sun = sun;
+        this.prune = prune;
+        this.water = water;
+        this.fertilize = fertilize;
+    }
+    
+    
+    
+}
