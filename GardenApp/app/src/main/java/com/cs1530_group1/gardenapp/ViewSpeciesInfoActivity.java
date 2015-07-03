2577ba53dd33b0ea3ae15486030d8fa5adc934d7
Charles Smith <cas275@pitt.edu>
2015-07-03 12:25:27 -0400

    Now populates with real information

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/ViewSpeciesInfoActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/ViewSpeciesInfoActivity.java
@@ -1,17 +1,75 @@
 package com.cs1530_group1.gardenapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
+
+
+@SuppressWarnings("deprecation")
 public class ViewSpeciesInfoActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = "Species Info Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_species_info);
+
+        //get the garden from App class
+        Garden garden = ((App) getApplication()).getGarden();
+
+        //get the speciesName passed from the last activity
+        String speciesName = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+
+
+        if(speciesName == null||speciesName.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Error loading information", Toast.LENGTH_SHORT).show();
+            Log.e(LOG_TAG, "received a null or empty string, cannot display relevant information");
+            return;
+        }
+
+        //get the name TextView and append the species name
+        TextView name = (TextView) findViewById(R.id.species_name);
+        name.append("\n" + speciesName);
+
+        //get the description TextView and append info
+        TextView descrpit = (TextView)findViewById(R.id.species_descript);
+        descrpit.append("\n" + garden.getDescription(speciesName));
+
+        //set type TextView
+        TextView type = (TextView)findViewById(R.id.species_type);
+        type.append("\n" + garden.getSpeciesType(speciesName));
+
+        //set sun TextView
+        TextView sun = (TextView)findViewById(R.id.species_sun);
+        sun.append("\n" + garden.getSunLevel(speciesName));
+
+        //set plant date TextView
+        TextView plantDate = (TextView)findViewById(R.id.species_plant_date);
+        plantDate.append("\n" + garden.getPlantDate(speciesName));
+
+        //set prune date TextView
+        TextView pruneDate = (TextView)findViewById(R.id.species_prune_date);
+        pruneDate.append("\n"+garden.getPruneDate(speciesName));
+
+        //set size TextView
+        TextView size = (TextView)findViewById(R.id.species_size);
+        size.append("\n" + garden.getSize(speciesName));
+
+        //set color TextView
+        TextView color = (TextView)findViewById(R.id.species_color);
+        color.append("\n" + Integer.toHexString(garden.getColor(speciesName)));
+
+
+
     }
 
     @Override
