92841f6753d1e978c9490f42eafed2373204adc7
Charles Smith <cas275@pitt.edu>
2015-07-03 00:33:47 -0400

    fixed a problem with a hard coded string in MainActivity.java

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/MainActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
 
         //todo replace this with a call to storage to get the saved garden/create a new one
         //start fake data
-        String gardenString = "2:tomato:half:23:44:1234:sunflower:full:34:54:1245:1:2:3:sunflower";
+        String gardenString = "2-tomato-a tomato species-Annual-01/23/1993 23:25:12-03/03/1993 21:22:13-high-25-33-sunflower-a sunny flower-Perennial-02/21/1986 22:42:12-04/12/1980 23:45:12-low-32-12-1-23-43-tomato";
         //end fake data
         //todo this shouldn't happen in the main thread
         Garden garden = Garden.stringToGarden(gardenString);
