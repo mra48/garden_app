a4b871faca99e74c0029535d6c5877b06e80486a
Charles Smith <cas275@pitt.edu>
2015-07-07 14:09:35 -0400

    removed unused String

--- GardenApp/app/src/androidTest/java/com/cs1530_group1/gardenapp/MainActivityTest.java
+++ GardenApp/app/src/androidTest/java/com/cs1530_group1/gardenapp/MainActivityTest.java
@@ -29,7 +29,7 @@ public class MainActivityTest extends TestCase {
     }
 
     public void testLoadDefaultGarden() throws Exception {
-        String testString = "2-tomato-a tomato species-Annual-01/23/1993 11:25:12-03/03/1993 09:22:13-high-25-33-sunflower-a sunny flower-Perennial-02/21/1986 10:42:12-04/12/1980 11:45:12-low-32-12-1-23-43-tomato";
+        //String testString = "2-tomato-a tomato species-Annual-01/23/1993 11:25:12-03/03/1993 09:22:13-high-25-33-sunflower-a sunny flower-Perennial-02/21/1986 10:42:12-04/12/1980 11:45:12-low-32-12-1-23-43-tomato";
         MainActivity activity = new MainActivity();
         App app = new App();
         activity.loadDefaultGarden(app);
