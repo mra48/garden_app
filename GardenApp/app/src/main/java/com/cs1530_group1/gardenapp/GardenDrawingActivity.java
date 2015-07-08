a47b652f3fd250b5e1a81919ee8f84360481f0da
grselker@gmail.com <grselker@gmail.com>
2015-07-08 19:41:51 -0400

    Changes to add menu bar.

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/GardenDrawingActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/GardenDrawingActivity.java
@@ -1,12 +1,20 @@
 package com.cs1530_group1.gardenapp;
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * GardenDrawingActivity : the graphical front end for drawing plants on top of the
  * garden layout picture.
  */
-public class GardenDrawingActivity extends Activity {
+@SuppressWarnings("deprecation")
+public class GardenDrawingActivity extends ActionBarActivity {
+   //Log tag, for logging errors
+   private static final String LOG_TAG = "Garden Drawing Activity";
 
     /**
      * onCreate : creates a new GardenView
@@ -21,4 +29,30 @@ public class GardenDrawingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(new GardenView(this, g));
     }
+
+
+public boolean onCreateOptionsMenu(Menu menu)
+{
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_garden_drawing, menu);
+    return true;
+}
+
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    if (id == R.id.species_list) {
+        Log.v(LOG_TAG, "Starting SpeciesListActivity");
+        Intent intent = new Intent(this, SpeciesListActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+}
+
 }
