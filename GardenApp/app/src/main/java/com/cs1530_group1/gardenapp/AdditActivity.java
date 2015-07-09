3a228078bff891fb174b9594537db9a0781720c1
Charles Smith <chuckie512@gmail.com>
2015-07-08 21:26:27 -0400

    added methods for click actions

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/AdditActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/AdditActivity.java
@@ -2,12 +2,17 @@ package com.cs1530_group1.gardenapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 @SuppressWarnings("deprecation")
 public class AdditActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = "AdditActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,25 @@ public class AdditActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void cancel(View view) {
+        Log.e(LOG_TAG, "cancel is not yet implemented");
+        Toast.makeText(getApplicationContext(), "This feature is not yet implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    public void update(View view) {
+        Log.e(LOG_TAG, "update is not yet implemented");
+        Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    public void startColorPicker(View view) {
+        Log.e(LOG_TAG, "colorPicker is not yet implemented");
+        Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    public void startDatePicker(View view) {
+        Log.e(LOG_TAG, "datePicker is not yet implemented");
+        Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
     }
 }
