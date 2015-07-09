5d0e2761517d31a9f454caea2914d2f1ede579c9
Charles Smith <chuckie512@gmail.com>
2015-07-08 21:47:00 -0400

    comments and skeleton methods

--- GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/AdditActivity.java
+++ GardenApp/app/src/main/java/com/cs1530_group1/gardenapp/AdditActivity.java
@@ -1,5 +1,6 @@
 package com.cs1530_group1.gardenapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,12 +14,35 @@ import android.widget.Toast;
 public class AdditActivity extends ActionBarActivity {
 
     private static final String LOG_TAG = "AdditActivity";
+    private boolean editMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addit);
+
+        Intent intent = getIntent();
+        String speciesName = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(speciesName!=null){
+            setupEditMode(speciesName);
+        }
+        Log.e(LOG_TAG, "this activity is not yet ready for release");
+
+
+
     }
+
+    /**
+     * seting up the edit mode involves filling the text fields with old data
+     * @param speciesName the name of the species to be modified
+     */
+    protected void setupEditMode(String speciesName){
+        editMode = true;
+        Log.v(LOG_TAG, "we're editing" + speciesName);
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,21 +66,39 @@ public class AdditActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * called when the user presses cancel.
+     * Should return to the list activity and make no changes
+     * @param view unused
+     */
     public void cancel(View view) {
         Log.e(LOG_TAG, "cancel is not yet implemented");
         Toast.makeText(getApplicationContext(), "This feature is not yet implemented", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * makes the changes specified by the user.
+     * sends user to the viewInfo activity
+     * @param view unused
+     */
     public void update(View view) {
         Log.e(LOG_TAG, "update is not yet implemented");
         Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * this should launch the color picker activity, so the user can pick the color to represent that plant on the gardenView
+     * @param view unused
+     */
     public void startColorPicker(View view) {
         Log.e(LOG_TAG, "colorPicker is not yet implemented");
         Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * let's the user pick the date used as a species plantdate
+     * @param view unused
+     */
     public void startDatePicker(View view) {
         Log.e(LOG_TAG, "datePicker is not yet implemented");
         Toast.makeText(getApplicationContext(),"This feature is not yet implemented", Toast.LENGTH_SHORT).show();
